cc5b3c2224624df9b08df6544e4a8fd29c783bbb
VinodPerumal <vinod.perumal.vp@gmail.com>
2017-08-02 23:19:42 +0200

    initial

---
+++ script.js
@@ -0,0 +1,64 @@
+function showrate()
+{
+  // Storing grocery items in a array
+  var selected_items = document.forms[0];
+  var txt = "";
+  var i;
+  var chosenitems = [];
+  for (i = 0; i < selected_items.length; i++) {
+    if (selected_items[i].checked) {
+      txt = txt + selected_items[i].value + " ";
+      chosenitems.push(selected_items[i].value);
+    }
+  }
+  console.log("You are searching for items" + txt);
+
+  // Storing shops in a array
+  var selected_items = document.forms[1];
+  var txt = "";
+  var i;
+  var chosenshops = [];
+  for (i = 0; i < selected_items.length; i++) {
+    if (selected_items[i].checked) {
+      txt = txt + selected_items[i].value + " ";
+      chosenshops.push(selected_items[i].value);
+    }
+  }
+  console.log("You are searching in shops " + txt);
+
+  //per chosenitem per shop
+  var i,j;
+  for (i = 0; i < chosenitems.length; i++) {
+    for (j = 0; j < chosenshops.length; j++) {
+        openshoplink(chosenitems[i],chosenshops[j]);
+    }
+  }
+}
+
+function openshoplink(itemname,shopname){
+
+  switch (shopname) {
+    case "Albert-Heijn" :
+        findinah(itemname);
+    case 5:
+        text = "Soon it is Weekend";
+        break;
+    case 0:
+    case 6:
+        text = "It is Weekend";
+        break;
+    default:
+        text = "Looking forward to the Weekend";
+  }
+}
+
+function findinah(itemname){
+
+  var url = "https://www.ah.nl/?gclid=CPDgkde2udUCFW0R0wod6KsHzw";
+  var win = window.open(url, '_blank');
+  win.focus();
+  searchfield = document.getElementsByClassName('paragraph--1 navigation-search__input');
+  searchfield.innerHTML = itemname;
+
+
+}
