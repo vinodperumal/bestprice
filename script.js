6916a594c590045a50bfedbff3c45137e511082e
VinodPerumal <vinod.perumal.vp@gmail.com>
2017-08-04 23:30:15 +0200

    04aug-2330

--- script.js
+++ script.js
@@ -1,28 +1,77 @@
-function showproduct(){
+function showproduct(shopname,checkbox){
   console.log("in showproduct");
-  let count = 0;
-  let checkeditem = document.getElementsByClassName('groceryitems');
 
-  //counting checkeditems
-  for (var i = 0; i < checkeditem.length; i++) {
+  let searchitem = document.getElementById('searchitem').value;
+  let itemlen = searchitem.length;
+  let searchstring = "";
 
-    if (checkeditem[i].checked) {
-        console.log("checkeditem " + checkeditem[i]);
-        receiveditem = checkeditem[i].value;
-        count = count + 1;
-        }
+  console.log(itemlen);
+  console.log(shopname);
+
+  if ( ( itemlen > 0 ) && (checkbox.checked == true) ){
+
+    switch(shopname){
+
+      case "Albert-Heijn" :
+
+        searchstring = 'https://www.ah.nl/zoeken?rq='+ searchitem + '&searchType=global';
+        console.log(searchstring);
+        document.getElementById('ahframe').src = searchstring ;
+        document.getElementById('showshops').style.display = 'inline-block';
+        document.getElementById('showAH').style.display = 'inline';
+        break;
+
+      case "Dirk" :
+
+        searchstring = 'https://boodschappen.dirk.nl/zoeken?search='+ searchitem;
+        console.log(searchstring);
+        document.getElementById('dirkframe').src = searchstring ;
+        document.getElementById('showshops').style.display = 'inline-block';
+        document.getElementById('showDirk').style.display = 'inline';
+        break;
+
+      case "Jumbo" :
+
+        searchstring = 'https://www.jumbo.com/zoeken?SearchTerm='+ searchitem;
+        console.log(searchstring);
+        window.open(searchstring,'_blank');
+        break;
+
+      default :
+
+        alert('wrong input');
+        break;
+
     }
-  console.log(count);
-  if (count>0){
-    let searchstring = 'https://www.ah.nl/zoeken?rq='+ receiveditem + '&searchType=global';
-    document.getElementById('showshops').style.display = 'inline-block';
-    document.getElementById('showAH').style.display = 'inline-block';
-    document.getElementById('ahframe').src = searchstring ;
-  }else {
-    alert ("please check a item");
-  }
-}
 
-function hello(){
-  console.log("hello");
+  }else if((checkbox.checked == false)){
+
+    switch(shopname){
+
+      case "Albert-Heijn" :
+        document.getElementById('showAH').style.display = 'none';
+        break;
+
+      case "Dirk" :
+
+        document.getElementById('showDirk').style.display = 'none';
+        break;
+
+      case "Jumbo" :
+
+      document.getElementById('showUnknown').style.display = 'none';
+      break;
+
+
+      default :
+
+      alert('wrong input');
+      break;
+
+  }
+
+  }else {
+        alert ("please provide an item to search");
+      }
+
 }
