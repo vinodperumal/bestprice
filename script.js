db8dfb39904b04c56ab8caa00117a004a8fe5710
perumalv <vinod.perumal@rabobank.nl>
2017-08-04 18:03:51 +0200

    04Aug17-1800

--- script.js
+++ script.js
@@ -1,67 +1,28 @@
-function showrate()
-{
-  // Storing grocery items in a array
-  var selected_items = document.forms[0];
-  var txt = "";
-  var i;
-  var chosenitems = [];
-  for (i = 0; i < selected_items.length; i++) {
-    if (selected_items[i].checked) {
-      txt = txt + selected_items[i].value + " ";
-      chosenitems.push(selected_items[i].value);
-    }
-  }
-  console.log("You are searching for items" + txt);
+function showproduct(){
+  console.log("in showproduct");
+  let count = 0;
+  let checkeditem = document.getElementsByClassName('groceryitems');
 
-  // Storing shops in a array
-  var selected_items = document.forms[1];
-  var txt = "";
-  var i;
-  var chosenshops = [];
-  for (i = 0; i < selected_items.length; i++) {
-    if (selected_items[i].checked) {
-      txt = txt + selected_items[i].value + " ";
-      chosenshops.push(selected_items[i].value);
-    }
-  }
-  console.log("You are searching in shops " + txt);
+  //counting checkeditems
+  for (var i = 0; i < checkeditem.length; i++) {
 
-  //per chosenitem per shop
-  var i,j;
-  for (i = 0; i < chosenitems.length; i++) {
-    for (j = 0; j < chosenshops.length; j++) {
-        openshoplink(chosenitems[i],chosenshops[j]);
+    if (checkeditem[i].checked) {
+        console.log("checkeditem " + checkeditem[i]);
+        receiveditem = checkeditem[i].value;
+        count = count + 1;
+        }
     }
+  console.log(count);
+  if (count>0){
+    let searchstring = 'https://www.ah.nl/zoeken?rq='+ receiveditem + '&searchType=global';
+    document.getElementById('showshops').style.display = 'inline-block';
+    document.getElementById('showAH').style.display = 'inline-block';
+    document.getElementById('ahframe').src = searchstring ;
+  }else {
+    alert ("please check a item");
   }
 }
 
-function openshoplink(itemname,shopname){
-
-  switch (shopname) {
-    case "Albert-Heijn" :
-        findinah(itemname);
-    case 5:
-        text = "Soon it is Weekend";
-        break;
-    case 0:
-    case 6:
-        text = "It is Weekend";
-        break;
-    default:
-        text = "Looking forward to the Weekend";
-  }
-}
-
-function findinah(itemname){
-
-  var url = "https://www.ah.nl/";
-  var win = window.open(url, '_blank');
-  win.focus();
-  $(document).ready(function(){
-
-     // jQuery methods go here...
-     $("*").$("[name='rq']");
-
-  });
-
+function hello(){
+  console.log("hello");
 }
