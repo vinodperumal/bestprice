8b9bd02744059f67a30c9adf2facfee118391928
perumalv <vinod.perumal@rabobank.nl>
2017-08-07 16:11:28 +0200

    05Aug17-16:11

--- script.js
+++ script.js
@@ -152,3 +152,38 @@ function resetall(){
     document.getElementById('Select All').checked = false;
     document.getElementById('searchitem').value = '';
  }
+
+function addScript() {
+  console.log('in addScript');
+
+  var s = document.createElement( 'script' );
+  s.setAttribute( 'src', "https://maps.googleapis.com/maps/api/js?key=AIzaSyD4t3Mysvz9tv2U_5ZlYdSbp3rR0LFp_KA&libraries=places&callback=initialize" );
+  document.body.appendChild( s );
+}
+
+var geocoder;
+var map;
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(-34.397, 150.644);
+  var mapOptions = {
+  zoom: 8,
+  center: latlng
+  }
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  }
+
+function codeAddress() {
+  var address = document.getElementById('postcode').value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+  if (status == 'OK') {
+    map.setCenter(results[0].geometry.location);
+    var marker = new google.maps.Marker({
+    map: map,
+    position: results[0].geometry.location
+  });
+  } else {
+    alert('Geocode was not successful for the following reason: ' + status);
+  }
+  });
+}
